d3dae0beeed16479e40934ce93485be243345a44
WorkingDada <6430222521@student.chula.ac.th>
2024-04-10 23:47:28 +0700

    f

--- MyFS.py
+++ MyFS.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # 2110352 Operating System
 # FUSE/Filesystem exercise
@@ -9,16 +9,32 @@
 import os, stat, errno
 import fuse
 from fuse import Fuse
+import requests
 
 if not hasattr(fuse, '__version__'):
-    raise RuntimeError, \
-        "your fuse-py doesn't know of fuse.__version__, probably it's too old."
+    raise RuntimeError("your fuse-py doesn't know of fuse.__version__, probably it's too old.")
 
 fuse.fuse_python_api = (0, 2)
 
 containers={ \
-		'/subject':"2018S1 - Operating Systems\nCP ENG CU\n" \
-	}
+            '/subject':b"2018S1 - Operating Systems\nCP ENG CU\n",
+            '/instructors': b"0:CP ENG CU OS 2018S1 - Instructors\n1:    Thongchai Rojkandsadan\n2:    Veera Muangsin, Ph.D.\n3:    Krerk Piromsopa, Ph.D.\n",
+            '/students': b"0:CP ENG CU OS 2018S1 - Students, Group Name: fbb\n1:    6430222521 Pattapon Vichanukroh \n2:    6431331421 Punnawich Yiamsombat \n",
+            '/participation': b""
+            }
+
+def myRead():
+    req=requests.get('https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/status.php')
+    content = bytes(req.text, 'utf-8')
+    return content
+
+def myWrite(buf):
+    decoded_buf = buf.decode('utf-8')
+    raw=decoded_buf.split(':')
+    checkInUrl='https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/checkIn.php'
+    params={'studentid': raw[0], 'name': raw[1], 'email': raw[2]}
+    rpost = requests.post(checkInUrl, data=params)
+    return len(buf)
 
 class MyStat(fuse.Stat):
     def __init__(self):
@@ -26,8 +42,8 @@ class MyStat(fuse.Stat):
         self.st_ino = 0
         self.st_dev = 0
         self.st_nlink = 0
-        self.st_uid = 0
-        self.st_gid = 0
+        self.st_uid = 1000
+        self.st_gid = 1000
         self.st_size = 0
         self.st_atime = 0
         self.st_mtime = 0
@@ -38,12 +54,16 @@ class MyFS(Fuse):
     def getattr(self, path):
         st = MyStat()
         if path == '/':
-            st.st_mode = stat.S_IFDIR | 0777
+            st.st_mode = stat.S_IFDIR | 0o0777
             st.st_nlink = 2
         elif path in containers:
-            st.st_mode = stat.S_IFREG | 0444
+            st.st_mode = stat.S_IFREG | 0o0444
             st.st_nlink = 1
-            content=containers[path]
+            if path == '/participation':
+                st.st_mode = stat.S_IFREG | 0o0666
+                content=myRead()
+            else:
+                content=containers[path]
             st.st_size = len(content)
         else:
             return -errno.ENOENT
@@ -54,22 +74,23 @@ class MyFS(Fuse):
         for r in  '.', '..':
             yield fuse.Direntry(r)
 
+
         for r in filenames:
             yield fuse.Direntry(r[1:])
-
 
     def open(self, path, flags):
         if path not in containers:
             return -errno.ENOENT
-        accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
-        if (flags & accmode) != os.O_RDONLY:
-            return -errno.EACCES
 
     def read(self, path, size, offset):
         if path not in containers:
             return -errno.ENOENT
 
-        content = containers[path]
+        if path=='/participation':
+            content = myRead()
+        else:
+            content = containers[path]
+
         slen = len(content)
         if offset < slen:
             if offset + size > slen:
@@ -78,6 +99,11 @@ class MyFS(Fuse):
         else:
             buf = ''
         return buf
+    def write(self, path, buf, offset):
+        if path=='/participation':
+            return myWrite(buf)
+
+        return -errno.ENOENT
 
 def main():
     usage="""
@@ -85,8 +111,8 @@ MyFS mounting_point
 
 """ + Fuse.fusage
     server = MyFS(version="%prog " + fuse.__version__,
-                     usage=usage,
-                     dash_s_do='setsingle')
+                  usage=usage,
+                  dash_s_do='setsingle')
 
     server.parse(errex=1)
     server.main()
